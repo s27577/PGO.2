a5109249a600c48c210425fa67748785672c4530
s27577 <163741382+s27577@users.noreply.github.com>
2024-06-02 15:13:14 +0200

    Update Employee.java

--- PGO.2/src/Employee.java
+++ PGO.2/src/Employee.java
@@ -1,2 +1,20 @@
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String email;
+    private int PESEL;
+    private int yearsOfEmployment;
+
+public Employee(String firstName, String lastName, String address, String email, int PESEL, int yearsOfEmployment) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.address = address;
+    this.email = email;
+    this.PESEL = PESEL;
+    this.yearsOfEmployment = yearsOfEmployment;
+}
+    public double calculateSalary() {
+        return 3000 + (this.yearsOfEmployment * 1000);
+    }
 }
