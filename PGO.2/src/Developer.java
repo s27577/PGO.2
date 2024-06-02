ba71816750dda203f1af0fcf4534df1d3672768f
s27577 <163741382+s27577@users.noreply.github.com>
2024-06-02 15:12:55 +0200

    Update Developer.java

--- PGO.2/src/Developer.java
+++ PGO.2/src/Developer.java
@@ -1,2 +1,20 @@
-public class Developer {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Developer extends Employee {
+    private List<Technology> technologies;
+
+    public Developer(String firstName, String lastName, String address, String email, int PESEL, int yearsOfEmployment, List<Technology> technologies) {
+        super(firstName, lastName, address, email, PESEL, yearsOfEmployment);
+        this.technologies = technologies;
+    }
+
+    @Override
+    public double calculateSalary() {
+        double bonus = 0;
+        for (Technology technology : technologies) {
+            bonus += technology.getBonus();
+        }
+        return super.calculateSalary() + bonus;
+    }
 }
