1ec306cf0c88ce13ffe1e7200cc3ff76ba57a44c
s27577 <163741382+s27577@users.noreply.github.com>
2024-06-02 15:15:45 +0200

    Add files via upload

---
+++ PGO.2/src/TestType.java
@@ -0,0 +1,17 @@
+public class TestType {
+    private String name;
+    private double bonus;
+
+    public TestType(String name, double bonus) {
+            this.name = name;
+            this.bonus = bonus;
+    }
+
+    public String getName() {
+            return name;
+    }
+
+    public double getBonus() {
+            return bonus;
+    }
+}
