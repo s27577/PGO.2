55b767261dee4f433b9ac4f0bb8309d1960f05a5
s27577 <163741382+s27577@users.noreply.github.com>
2024-06-02 15:13:49 +0200

    Update Main.java

--- PGO.2/src/Main.java
+++ PGO.2/src/Main.java
@@ -1,29 +1,33 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         ArrayList<Employee> employees = new ArrayList<>();
 // Creating and adding developers
-        Developer dev = new Developer("John", "Doe", "New York", "john@doe.com", "123456789", 2020);
-        Technology t = new Technology("Java", 800);
-        dev.addTechnology(t);
+        List<Technology> developerTechnologies = new ArrayList<>();
+        Developer dev = new Developer("John", "Doe", "New York", "john@doe.com", 123456789, 2, developerTechnologies);
+        Technology t = new Technology("Python", 800);
+        developerTechnologies.add(t);
         employees.add(dev);
 // Creating and adding testers
-        Tester tester = new Tester("Jane", "Smith", "Los
-                Angeles", "jane@smith.com", "987654321", 2021);
-        tester.addTestType("UI/UX");
+        List<String> testerTestTypes = new ArrayList<>();
+        Tester tester = new Tester("Jane", "Smith", "Los Angeles", "jane@smith.com", 987654321, 3, testerTestTypes);
+        testerTestTypes.add("UI/UX");
         employees.add(tester);
 // Creating and adding a manager
-        Manager manager = new Manager("Bob", "Johnson",
-                "Chicago", "bob@johnson.com", "1122334455", 2019);
-        Goal g=new Goal(2010, 10, 15, "Implementing FB login",
-                1000);
-        manager.addGoals(g);
+        List<Goal> managerGoals = new ArrayList<>();
+        Manager manager = new Manager("Bob", "Johnson", "Chicago", "bob@johnson.com", 1122334455, 5, managerGoals);
+        Goal g = new Goal("Implementing FB login",LocalDate.of(2022, 4, 15), 1500);
+        managerGoals.add(g);
         employees.add(manager);
 // Calculating total amount to be paid
         int totalAmount = 0;
         for (Employee employee : employees) {
             totalAmount += employee.calculateSalary();
         }
-        System.out.println("Total amount to be paid this month:
-                " + totalAmount + " USD");
+        System.out.println("Total amount to be paid this month: " + totalAmount + " USD");
     }
 }
