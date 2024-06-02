f4cb8bdf29a9a6abf6817453373c6f3142fd136d
s27577 <163741382+s27577@users.noreply.github.com>
2024-06-02 15:14:09 +0200

    Update Manager.java

--- PGO.2/src/Manager.java
+++ PGO.2/src/Manager.java
@@ -1,2 +1,22 @@
-public class Manager {
+import java.util.List;
+import java.time.LocalDate;
+
+public class Manager extends Employee {
+    private List<Goal> goals;
+
+    public Manager(String firstName, String lastName, String address, String email, int PESEL, int yearsOfEmployment, List<Goal> goals) {
+        super(firstName, lastName, address, email, PESEL, yearsOfEmployment);
+        this.goals = goals;
+    }
+
+    public double calculateSalary() {
+        double bonus = 0;
+        LocalDate now = LocalDate.now();
+        for (Goal goal : goals) {
+            if (goal.getDate().getMonthValue() == now.getMonthValue() && goal.getDate().getYear() == now.getYear()) {
+                bonus += goal.getBonus();
+            }
+        }
+        return super.calculateSalary() + bonus;
+    }
 }
