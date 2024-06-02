a03752b1df52bea6d8162585f5562739dc5042b5
s27577 <163741382+s27577@users.noreply.github.com>
2024-06-02 15:13:31 +0200

    Update Goal.java

--- PGO.2/src/Goal.java
+++ PGO.2/src/Goal.java
@@ -1,2 +1,23 @@
+import java.time.LocalDate;
 public class Goal {
+    private String name;
+    private LocalDate date;
+    private int bonus;
+
+    public Goal(String name, LocalDate date, int bonus) {
+        this.name = name;
+        this.date = date;
+        this.bonus = bonus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+    public int getBonus() {
+        return bonus;
+    }
 }
